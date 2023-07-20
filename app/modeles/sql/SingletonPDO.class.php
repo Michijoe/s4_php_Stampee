17b8c308d0f3e2d1ec071bc4c9f489e0f2aaca42
Michijoe <e2296540@cmaisonneuve.qc.ca>
2023-07-20 11:21:15 -0400

    webdev

--- app/modeles/sql/SingletonPDO.class.php
+++ app/modeles/sql/SingletonPDO.class.php
@@ -5,10 +5,10 @@ class SingletonPDO extends PDO
   private static $instance = null;
 
   const DB_SERVEUR  = 'localhost';
-  const DB_NOM      = 'stampee';
+  const DB_NOM      = (ENV == "DEV") ?  'stampee' : 'e2296540';
   const DB_DSN      = 'mysql:host=' . self::DB_SERVEUR . ';dbname=' . self::DB_NOM . ';charset=utf8';
-  const DB_LOGIN    = 'root';
-  const DB_PASSWORD = 'root';
+  const DB_LOGIN    = (ENV == "DEV") ?  'root' : 'e2296540';
+  const DB_PASSWORD = (ENV == "DEV") ?  'root' : 'iloIGaO6rJ1ZYQq8gKTl';
 
   private function __construct()
   {
